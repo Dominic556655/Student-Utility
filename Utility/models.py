6e161681aa017393fa5c215e21fb93de65038db4
Dominic <ichadominic5@gmail.com>
2026-07-17 14:15:53 +0100

    Update models.py

--- Utility/models.py
+++ Utility/models.py
@@ -38,12 +38,19 @@ class GPARecord(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     gpa = models.FloatField()
     total_units = models.IntegerField()
+
+    grading_scale = models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+        default=5.0
+    )
+
     classification = models.CharField(max_length=50)
     courses = models.JSONField(default=list)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username} - {self.gpa}"
+        return f"{self.user.username} - {self.gpa}/{self.grading_scale}"
     
 
 from django.db import models
